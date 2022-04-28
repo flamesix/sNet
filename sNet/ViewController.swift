07abbe9d25930863f18e1bbefe8cca92f883e09a
grinevich <grinevichyu@gmail.com>
2022-04-28 09:37:32 +0300

    initial commit

---
+++ sNet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sNet
+//
+//  Created by Юрий Гриневич on 28.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

52514d48fdbba67495180f2ad917f1307ff41017
grinevich <grinevichyu@gmail.com>
2022-05-05 12:56:58 +0300

    setting secondLoginVC

--- sNet/Controllers/SecondLoginViewController.swift
+++ sNet/Controllers/SecondLoginViewController.swift
@@ -27,8 +27,8 @@ class SecondLoginViewController: UIViewController {
         passwordTextField.rightView = showHidePasswordButton
         passwordTextField.rightViewMode = .always
     
-        
-        
+        self.setupHideKeyboardOnTap()
+        registerForKeyboardNotifications()
     }
     
     @objc func showHidePassword(sender: UIButton!) {
@@ -39,6 +39,39 @@ class SecondLoginViewController: UIViewController {
                    sender.setImage(UIImage(systemName: "eye.slash.fill"), for: .normal)
                }
      }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector:
+                                                #selector(keyboardWasShown(_:)),
+                                               name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector:
+                                                #selector(keyboardWillBeHidden(_:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
+        
+    }
+    
+    @objc func keyboardWasShown(_ notificiation: NSNotification) {
+        guard let info = notificiation.userInfo,
+              let keyboardFrameValue =
+                info[UIResponder.keyboardFrameBeginUserInfoKey]
+                as? NSValue else { return }
+        
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+        
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0,
+                                         bottom: keyboardSize.height, right: 0.0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    @objc func keyboardWillBeHidden(_ notification:
+                                    NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
     
     @IBAction func enterButtonPressed(_ sender: UIButton) {
     }

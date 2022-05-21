31a0d4a5b3023e6d5531f238410cc80275b02519
grinevich <grinevichyu@gmail.com>
2022-05-21 09:15:30 +0300

    fixed constrains alerts in console

--- sNet/View/NewsAndPhotoTableViewCell.swift
+++ sNet/View/NewsAndPhotoTableViewCell.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class NewsAndPhotoTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var userPhotoImage: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var dateCreatedLabel: UILabel!
+    @IBOutlet weak var newsTextLabel: UILabel!
+    
+    @IBOutlet weak var firstNewsImage: UIImageView!
+    @IBOutlet weak var secondNewsImage: UIImageView!
+    @IBOutlet weak var thirdNewsImage: UIImageView!
+    @IBOutlet weak var forthNewsImage: UIImageView!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var commentButton: UIButton!
+    @IBOutlet weak var repostButton: UIButton!
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
@@ -20,4 +37,6 @@ class NewsAndPhotoTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    
+    
 }

ac387c627291473a00b38bb0a0f53e581a13b7eb
grinevich <grinevichyu@gmail.com>
2022-05-23 16:32:48 +0300

    added control to UIView in FriendsTVC

--- sNet/View/FriendsPhotoView.swift
+++ sNet/View/FriendsPhotoView.swift
@@ -7,9 +7,14 @@
 
 import UIKit
 
-class FriendsPhotoView: UIView {
+class FriendsPhotoView: UIControl {
 
     @IBOutlet var userPhotoImageView: UIImageView!
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupTap()
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -25,12 +30,34 @@ class FriendsPhotoView: UIView {
         
     }
     
-    
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
         // Drawing code
     }
     
+    
+
+    private func setupTap() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(friendsPhotoTap))
+        tap.numberOfTapsRequired = 1
+        addGestureRecognizer(tap)
+        
+    }
+    
+    
+    @objc private func friendsPhotoTap(_ tapGesture: UITapGestureRecognizer) {
+        
+        UIView.animate(withDuration: 0.4,
+                       delay: 0,
+                       usingSpringWithDamping: 0.1,
+                       initialSpringVelocity: 0.1,
+                       options: []) {
+            self.userPhotoImageView.transform = CGAffineTransform.init(scaleX: 1.2, y: 1.2)
+        } 
+
+        sendActions(for: .valueChanged)
+        
+    }
 
 }

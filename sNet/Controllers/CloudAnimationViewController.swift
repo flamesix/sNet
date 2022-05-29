7ca98f2be90b0581f9199f8358a4c9dd2e3575b9
grinevich <grinevichyu@gmail.com>
2022-05-29 18:55:30 +0300

    modified cloudAnimation

--- sNet/Controllers/CloudAnimationViewController.swift
+++ sNet/Controllers/CloudAnimationViewController.swift
@@ -21,9 +21,9 @@ class CloudAnimationViewController: UIViewController {
         layer.path = cloudLogo.cgPath
         layer.strokeStart = 0
         layer.strokeEnd = 1
-        layer.lineWidth = 2
-        layer.strokeColor = UIColor.magenta.cgColor
-        
+        layer.lineWidth = 10
+        layer.strokeColor = UIColor.lightGray.cgColor
+        layer.fillColor = UIColor.clear.cgColor
         
         cloudView.layer.addSublayer(layer)
     }
@@ -32,8 +32,8 @@ class CloudAnimationViewController: UIViewController {
         super.viewDidAppear(animated)
         
         let lineStartAnimation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeStart))
-        lineStartAnimation.fromValue = -10
-        lineStartAnimation.toValue = 1
+        lineStartAnimation.fromValue = -1
+        lineStartAnimation.toValue = 0
         lineStartAnimation.timingFunction = CAMediaTimingFunction(name: .easeInEaseOut)
         
         let lineEndAnimation = CABasicAnimation(keyPath: #keyPath(CAShapeLayer.strokeEnd))
@@ -54,7 +54,7 @@ class CloudAnimationViewController: UIViewController {
         }
     }
     
-    
+    /*
     let cloudLogo: UIBezierPath = {
         let bezierPath = UIBezierPath()
         
@@ -127,5 +127,25 @@ class CloudAnimationViewController: UIViewController {
         bezierPath.stroke()
 
         return bezierPath
+    }()
+    */
+    
+    let cloudLogo: UIBezierPath = {
+        
+    let bezier2Path = UIBezierPath()
+    bezier2Path.move(to: CGPoint(x: 164.1, y: 46.28))
+    bezier2Path.addCurve(to: CGPoint(x: 164.1, y: 129.72), controlPoint1: CGPoint(x: 197.3, y: 69.32), controlPoint2: CGPoint(x: 197.3, y: 106.68))
+    bezier2Path.addCurve(to: CGPoint(x: 87.68, y: 145.91), controlPoint1: CGPoint(x: 143.34, y: 144.13), controlPoint2: CGPoint(x: 114.5, y: 149.53))
+    bezier2Path.addCurve(to: CGPoint(x: 41.5, y: 167.5), controlPoint1: CGPoint(x: 69.56, y: 157.66), controlPoint2: CGPoint(x: 41.5, y: 167.5))
+    bezier2Path.addCurve(to: CGPoint(x: 56.33, y: 136.86), controlPoint1: CGPoint(x: 41.5, y: 167.5), controlPoint2: CGPoint(x: 51.16, y: 151.11))
+    bezier2Path.addCurve(to: CGPoint(x: 43.9, y: 129.72), controlPoint1: CGPoint(x: 51.95, y: 134.8), controlPoint2: CGPoint(x: 47.78, y: 132.42))
+    bezier2Path.addCurve(to: CGPoint(x: 43.9, y: 46.28), controlPoint1: CGPoint(x: 10.7, y: 106.68), controlPoint2: CGPoint(x: 10.7, y: 69.32))
+    bezier2Path.addCurve(to: CGPoint(x: 164.1, y: 46.28), controlPoint1: CGPoint(x: 77.09, y: 23.24), controlPoint2: CGPoint(x: 130.91, y: 23.24))
+    bezier2Path.close()
+    UIColor.red.setStroke()
+    bezier2Path.lineWidth = 9
+    bezier2Path.stroke()
+        
+        return bezier2Path
     }()
 }
